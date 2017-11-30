e04b7ec8ab6dc19d15a5e714d96afd9a39996234
PoliGomes <poliana.ph@gmail.com>
2017-11-30 21:09:30 -0200

    table done

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Hero from "./components/Hero/Hero";
 import Features from "./components/Features/Features"
 import Bring from "./components/Bring/Bring";
 import Footer from "./components/Footer/Footer";
+import Services from "./components/Services/Services";
 
 export default () => (
   <div className="App">
+    <Services />
     <Hero />
     <Features />
     <Bring />
