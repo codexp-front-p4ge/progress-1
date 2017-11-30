73e0e6b0cd0283f78da69ec0565ea955687d2e07
rafaeltanaka <rafaeltanaka@hotmail.com>
2017-11-30 20:27:51 -0200

    js e css EstimateHeader

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Hero from "./components/Hero/Hero";
 import Features from "./components/Features/Features"
 import Bring from "./components/Bring/Bring";
 import Footer from "./components/Footer/Footer";
+import EstimateHeader from "./components/EstimateHeader/EstimateHeader";
 
 export default () => (
   <div className="App">
     <Hero />
+    <EstimateHeader />
     <Features />
     <Bring />
     <Footer />
